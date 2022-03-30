094578524b6129c597b682720fcdfc690d0a6516
Hrithik Chugh <93709013+dudehrithik007@users.noreply.github.com>
2022-03-30 16:47:17 +0530

    Add files via upload
    Program to print the following pattern: 
    
    1
    22
    333
    4444
    55555
    
    The user enters the number of rows to be printed.
    
    Code contains comments to explain itself and input validations too.

---
+++ p3.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+void pattern(int n);
+
+void main()
+{
+    printf("\n\nProgram to print the following pattern: \n\n");
+
+    printf("1\n22\n333\n4444\n55555\n\n");
+
+    printf("Enter the number of rows of the pattern to be printed: ");
+    int n;
+    do // Input validation for "n".
+    {
+        scanf("%d", &n);
+        if(n < 0)
+        printf("Number of rows are whole numbers only. Enter again: ");
+    } while (n < 0);
+
+    printf("\nPattern: \n\n");
+
+    pattern(n);
+
+    printf("\n\n");
+}
+
+void pattern(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++) 
+        {
+            printf("%d", i);
+        }  
+        printf("\n");
+    } 
+}
